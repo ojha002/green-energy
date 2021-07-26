e34f355e56bc46b6bc3647c644b8ac038534c467
ojha002 <83961712+ojha002@users.noreply.github.com>
2021-07-26 17:19:02 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,40 @@
+// Load your images on page-load
+function preloader() {
+    const imagesPaths = [
+       "./img/photovoltaic-array.jpg",
+       "./img/energy.jpg",
+       "./img/compact-flourescent-bulb.jpg"
+    ];
+
+    const images = [];
+    for (let i = 0; i < imagesPaths.length; i++) {
+        images[i] = new Image();
+        images[i].src = imagesPaths[i];
+    }
+
+    // Images ready to be used:
+    console.log(`Preloaded images:\n\t${images[0].src}\n\t${images[1].src}\n\t${images[2].src}`);
+};    
+
+window.addEventListener("load", preloader);
+
+
 // getting references
+
 let $dc = document.getElementById('dynamic-content');
 
 
 // resource
+
 const data = {
     partial1: {
-        imageURL: 'img/energy.jpg',
+        imageURL: 'img/photovoltaic-array.jpg',
         imageAlt: '...',
         heading: 'Solar Energy',
         bodyText: 'It is important that government invests in the mass project on establishing the solar energy, so that the governemnt is able to distribute the energy to regular people. And also work with CMHC to work on gaving renuable/solar pannel in the roof of the new build houses. Should even go with providing this for the low income housing as the cost of electricityy, heating is going to more, but if they are switiched with soloar eneargy source then there is high change of improving the environment with the living standard of the normal citizens.'
     },
     partial2: {
-        imageURL: 'img/photovoltaic-array.jpg',
+        imageURL: 'img/energy.jpg',
         imageAlt: '...',
         heading: 'Wind Mill',
         bodyText: 'It is important that government invests in the mass project on establishing the wind mill in deiiferent part of Canada, so that the governemnt is able to distribute the energy to regular people. And also work with CMHC to work on changing the source of energy from miniral to solar. Should even go with providing this for the low income housing as the cost of electricityy, heating is going to more, but if they are switiched with green energy source then there is high change of improving the environment with the living standard of the normal citizens.'
@@ -25,11 +48,20 @@ const data = {
 };
 
 // create the initial page markup:
-let markup = `<img src="${data.partial1.imageURL}" alt="" />
+let markup = {partial1:`<img src="${data.partial1.imageURL}" alt="" />
                 <h2>${data.partial1.heading}</h2>
-                <p>${data.partial1.bodyText}</p>`;
+                <p>${data.partial1.bodyText}</p>`,
 
-$dc.innerHTML = markup;
+            partial2:`<img src="${data.partial2.imageURL}" alt="" />
+                <h2>${data.partial2.heading}</h2>
+                <p>${data.partial2.bodyText}</p>`,
+
+            partial3:`<img src="${data.partial3.imageURL}" alt="" />
+                <h2>${data.partial3.heading}</h2>
+                <p>${data.partial3.bodyText}</p>`,
+            };
+
+$dc.innerHTML = markup.partial1;
 
 
 
@@ -54,17 +86,29 @@ function handleClick(ev) {
         }
     }
 
-    // add attribute to the currently clicked element
-    // https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
     currentItem.setAttribute('id', 'active');
+    
+    // check if clicked, assign data 
+
+
+    if (ev.target == listItems[0] ) {
+        $dc.innerHTML = markup.partial1;
+    } 
+
+    if (ev.target == listItems[1] ) {
+        $dc.innerHTML = markup.partial2;
+    } 
+
+    if (ev.target == listItems[2] ) {
+        $dc.innerHTML = markup.partial3;
+    } 
 }
 
+
+
+
 // registering list items for click event:
-// listItems[0].addEventListener('click', handleClick);
-// listItems[1].addEventListener('click', handleClick);
-// listItems[2].addEventListener('click', handleClick);
 
 for (let i = 0; i < listItems.length; i++) {
     listItems[i].addEventListener('click', handleClick)};
 
-  
